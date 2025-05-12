ce50c1a6fb4467c63d952cbe2cf43efcfaf5f982
JP <jp@jp.nl>
2025-05-12 15:43:33 +0200

    Demo OO - encapsaluation

---
+++ demo/OdidoSolution/OoDemo/Human.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoDemo;
+
+class Human
+{
+    private int _age; // stores the actual value "backing field"
+    public int MyProperty { get; set; }
+
+    private string _firstName = "JP";
+    private string _surname = "tB";
+
+    // => delegate  "arrow function"
+
+    public string FullName => $"{_firstName} {_surname}";
+
+    //public string FullName
+    //{
+    //    get
+    //    {
+    //        return $"{_firstName} {_surname}";
+    //    }
+    //}
+
+
+    public int Age
+    {
+        get { return _age; }
+        set
+        {
+            if (value > 150)
+            {
+                throw new ArgumentException("value");
+            }
+
+            _age = value;
+        }
+    }
+
+
+    //public int GetAge()
+    //{
+    //    return _age;
+    //}
+
+    //public void SetAge(int newAge)
+    //{
+    //    if (newAge > 150)
+    //    {
+    //        throw new ArgumentException("newAge");
+    //    }
+
+    //    _age = newAge;
+    //}
+}
---
+++ demo/OdidoSolution/OoDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OoDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var jp = new Human();
+        jp.Age = 12345;
+        jp.FullName = "qq";
+
+
+        Console.WriteLine($"JP is {jp.Age} years old");
+    }
+}
