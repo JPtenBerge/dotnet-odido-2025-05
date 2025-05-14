29051c5a181d4a667815c625713201564e176215
JP <jp@jp.nl>
2025-05-14 09:54:19 +0200

    Demonstrated interfaces

---
+++ demo/OdidoSolution/InterfaceDemo/IEmployee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo;
+
+interface IEmployee
+{
+    string Name { get; set; }
+
+    decimal CalculateMonthlyPay();
+}
+
+// "class A extends class B"
+// "class A implements interface B"
+// "interface A extends interface B"
+
+class SalaryGuy : IEmployee
+{
+    public string Name { get; set; }
+    public decimal MonthlyWage { get; set; }
+
+    public decimal CalculateMonthlyPay()
+    {
+        return MonthlyWage;
+    }
+}
+
+class HourlyConsultant : IEmployee
+{
+    public string Name { get; set; }
+    public decimal HourlyWage { get; set; }
+    public int NrOfHoursPerWeek { get; set; }
+
+    public decimal CalculateMonthlyPay()
+    {
+        return HourlyWage * NrOfHoursPerWeek * 4.5M;
+    }
+}
---
+++ demo/OdidoSolution/InterfaceDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace InterfaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new IEmployee[3];
+
+            employees[0] = new SalaryGuy { Name = "John", MonthlyWage = 4000 };
+            employees[1] = new HourlyConsultant { Name = "Wesley", HourlyWage = 110M, NrOfHoursPerWeek = 30 };
+            employees[2] = new HourlyConsultant { Name = "Jane", HourlyWage = 130M, NrOfHoursPerWeek = 20 };
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.Name} makes EUR {employee.CalculateMonthlyPay()}/month");
+            }
+        }
+    }
+}
