17dd99771b5b36284280f39258f0ce5166d8076c
JP <jp@jp.nl>
2025-05-14 14:45:17 +0200

    Generics

---
+++ demo/OdidoSolution/GenericDemo/MyList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+                                // generic constraints
+    class MyList<TKey, TValue> where TKey : IComparable, ICloneable
+    {
+        public void Set(TKey key, TValue value)
+        {
+            //key.
+            //key.CompareTo()
+        }
+    }
+}
---
+++ demo/OdidoSolution/GenericDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+namespace GenericDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // array: iterable   fixed-size
+        //var myArray = new int[4];
+        //myArray[14] = 34;
+
+
+        //List<int> // array
+        //LinkedList<int>
+
+        // before generics
+        //var list = new ArrayList(); // please don't use this
+        //list.Add(4);
+        //list.Add(8);
+        //list.Add(15);
+        //list.Add(16);
+        //list.Add("qwertyui");
+
+        //foreach (var item in list)
+        //{
+        //    var number = (int)item;
+        //    Console.WriteLine(number * 4);
+        //}
+
+        // with generics
+        new List<string>();
+        new List<bool>();
+        new List<Program>();
+        new List<List<int>>();
+
+
+        //new Dictionary<int, Game>
+
+
+        var genericList = new List<int>();
+        genericList.Add(4);
+        genericList.Add(8);
+        genericList.Add(15);
+        genericList.Add(16);
+        //genericList.Add("qwertyui");
+
+        foreach (var item in genericList)
+        {
+            Console.WriteLine(item * 4);
+        }
+
+
+        var myList = new MyList<int, short>();
+
+        var myList2 = new MyList<string, short>();
+
+        //var myList3 = new MyList<Program, short>();
+        //myList.Set();
+    }
+}
