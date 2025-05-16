6f1e0fac84720aa3728ef43558de71f10c9f459c
JP <jp@jp.nl>
2025-05-16 11:56:23 +0200

    Implemented the Get one and Post

--- demo/OdidoSolution/WebDemo/Endpoints/ProductEndpoints.cs
+++ demo/OdidoSolution/WebDemo/Endpoints/ProductEndpoints.cs
@@ -11,13 +11,26 @@ namespace WebDemo.Endpoints
             var group = routes.MapGroup("api/min-products");
 
             group.MapGet("/", GetAll);
-            group.MapGet("/{id:int}", GetAll);
-            group.MapPost("/", GetAll);
+            group.MapGet("/{id:int}", Get);
+            group.MapPost("/", Post);
         }
 
         public static async Task<IEnumerable<ProductEntity>> GetAll(ProductContext context)
         {
             return await context.Products.ToArrayAsync();
+        }
+
+        public static async Task<Results<NotFound<string>, Ok<ProductEntity>>> Get(int id, ProductContext context)
+        {
+            var product = await context.Products.SingleOrDefaultAsync(x => x.Id == id);
+            return product is null ? TypedResults.NotFound($"Product with id {id} does not exist.") : TypedResults.Ok(product);
+        }
+
+        public static async Task<Created<ProductEntity>> Post(ProductEntity newProduct, ProductContext context)
+        {
+            context.Products.Add(newProduct);
+            await context.SaveChangesAsync();
+            return TypedResults.Created("", newProduct); // Id has been set
         }
 
         public static async Task<Results<NotFound<string>, UnprocessableEntity<string>, Ok<ProductEntity>>> Put(int id, ProductContext context)
